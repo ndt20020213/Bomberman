7b8fd06df7c45ae7f0d34016e5f18cae7166cb86
Phan Cong Thanh <phancongthanhvtvpvn@gmail.com>
2021-11-05 15:21:35 +0700

    Đưa Bomber class vào player package

--- src/uet/oop/bomberman/BombermanGame.java
+++ src/uet/oop/bomberman/BombermanGame.java
@@ -7,7 +7,7 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.stage.Stage;
-import uet.oop.bomberman.entities.Bomber;
+import uet.oop.bomberman.entities.player.Bomber;
 import uet.oop.bomberman.entities.Entity;
 import uet.oop.bomberman.entities.background.Grass;
 import uet.oop.bomberman.entities.background.Wall;
--- src/uet/oop/bomberman/entities/player/Bomber.java
+++ src/uet/oop/bomberman/entities/player/Bomber.java
@@ -1,10 +1,7 @@
-package uet.oop.bomberman.entities;
+package uet.oop.bomberman.entities.player;
 
-import javafx.scene.SnapshotParameters;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.paint.Color;
+import uet.oop.bomberman.entities.Entity;
 
 public class Bomber extends Entity {
 
