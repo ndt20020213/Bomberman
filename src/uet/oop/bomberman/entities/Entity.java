5651a6f7223c13fab5fc299230ba06f77e31bb24
Phan Cong Thanh <phancongthanhvtvpvn@gmail.com>
2021-11-05 15:09:12 +0700

    Thêm interface kết nối mạng cho Entity class

--- src/uet/oop/bomberman/entities/Entity.java
+++ src/uet/oop/bomberman/entities/Entity.java
@@ -1,30 +1,69 @@
 package uet.oop.bomberman.entities;
 
-import javafx.scene.SnapshotParameters;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.paint.Color;
+import uet.oop.bomberman.BombermanGame;
+//import uet.oop.bomberman.container.World;
 import uet.oop.bomberman.graphics.Sprite;
+import uet.oop.bomberman.network.IConnected;
+import uet.oop.bomberman.structure.Point;
+import uet.oop.bomberman.structure.Rect;
 
-public abstract class Entity {
-    //Tọa độ X tính từ góc trái trên trong Canvas
-    protected int x;
+public abstract class Entity implements IConnected {
+    //Khóa vật thể
+    private int key = -1;
 
-    //Tọa độ Y tính từ góc trái trên trong Canvas
-    protected int y;
+    //Tọa độ tính từ góc trái trên trong Canvas
+    protected Point position;
 
     protected Image img;
 
+    //protected final World world;
+
     //Khởi tạo đối tượng, chuyển từ tọa độ đơn vị sang tọa độ trong canvas
-    public Entity( int xUnit, int yUnit, Image img) {
-        this.x = xUnit * Sprite.SCALED_SIZE;
-        this.y = yUnit * Sprite.SCALED_SIZE;
+    public Entity(int xUnit, int yUnit, Image img) {
+        position = new Point(xUnit * Sprite.SCALED_SIZE, yUnit * Sprite.SCALED_SIZE);
         this.img = img;
+        //world = BombermanGame.world;
+    }
+
+    public Rect getRect() {
+        return new Rect(position.x, position.y, img.getWidth(), img.getHeight());
     }
 
     public void render(GraphicsContext gc) {
-        gc.drawImage(img, x, y);
+        gc.drawImage(img, position.x, position.y);
     }
+
     public abstract void update();
+
+    @Override
+    public final boolean setKey(int key) {
+        if (this.key < 0) {
+            this.key = key;
+            return true;
+        } return false;
+    }
+
+    @Override
+    public int getKey() {
+        return key;
+    }
+
+    @Override
+    public IConnected update(String status) {
+        String[] data = status.split(" ");
+        position.x = Integer.parseInt(data[0]);
+        position.y = Integer.parseInt(data[1]);
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return position.getX() + " " + position.getY();
+    }
+
+    public boolean impact(Entity other) {
+        return getRect().impact(other.getRect());
+    }
 }
---
+++ src/uet/oop/bomberman/network/IConnected.java
@@ -0,0 +1,67 @@
+package uet.oop.bomberman.network;
+
+/*
+import uet.oop.bomberman.entities.attack.Bomb;
+import uet.oop.bomberman.entities.attack.Flame;
+import uet.oop.bomberman.entities.background.Grass;
+import uet.oop.bomberman.entities.background.Portal;
+import uet.oop.bomberman.entities.background.Wall;
+import uet.oop.bomberman.entities.bricks.RedBrick;
+import uet.oop.bomberman.entities.bricks.WhiteBrick;
+import uet.oop.bomberman.entities.enemies.Balloom;
+import uet.oop.bomberman.entities.enemies.Oneal;
+import uet.oop.bomberman.entities.items.BombItem;
+import uet.oop.bomberman.entities.items.FlameItem;
+import uet.oop.bomberman.entities.items.SpeedItem;
+import uet.oop.bomberman.entities.player.Bomber;
+*/
+
+public interface IConnected {
+
+    boolean setKey(int key);
+
+    int getKey();
+
+    IConnected update(String string);
+
+    String toString();
+
+    /*
+    static IConnected getConnectedEntity(String className, String data) {
+        switch (className) {
+            case "Bomber":
+                return new Bomber(0, 0).update(data);
+
+            case "Wall":
+                return new Wall(0, 0).update(data);
+            case "Grass":
+                return new Grass(0, 0).update(data);
+            case "WhiteBrick":
+                return new WhiteBrick(0, 0).update(data);
+            case "RedBrick":
+                return new RedBrick(0, 0).update(data);
+
+            case "Bomb":
+                return new Bomb(0, 0, null, 0).update(data);
+            case "Flame":
+                return new Flame(0, 0, 0, 0, 0).update(data);
+
+            case "SpeedItem":
+                return new SpeedItem(0,0).update(data);
+            case "BombItem":
+                return new BombItem(0,0).update(data);
+            case "FlameItem":
+                return new FlameItem(0,0).update(data);
+
+            case "Balloom":
+                return new Balloom(0, 0).update(data);
+            case "Oneal":
+                return new Oneal(0, 0).update(data);
+
+            case "Portal":
+                return new Portal(0, 0).update(data);
+        }
+        return null;
+    }
+    */
+}
