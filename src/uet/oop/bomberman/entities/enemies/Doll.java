767f8b2010d7a1679e3073797bd674f5354af688
ndt20020213 <20020213@vnu.edu.vn>
2021-12-06 15:57:58 +0700

    update di chuyển enemy

---
+++ src/uet/oop/bomberman/entities/enemies/Doll.java
@@ -0,0 +1,74 @@
+package uet.oop.bomberman.entities.enemies;
+
+import javafx.scene.canvas.GraphicsContext;
+import uet.oop.bomberman.graphics.Sprite;
+import uet.oop.bomberman.structure.Cell;
+import uet.oop.bomberman.structure.Point;
+
+public class Doll extends Enemy {
+    public static final double speed = 1;
+
+    public Doll(int xUnit, int yUnit) {
+        super(xUnit, yUnit, Sprite.balloom_right1.getFxImage());
+    }
+
+    @Override
+    public void update() {
+        Cell cell = super.getUnit();
+        if (Move.isEmpty()) {
+            MoveRandom();
+        }
+        if (Move.peek() == null) {
+            return;
+        }
+        Point move = new Point(Move.peek());
+        if (position.x < move.x) {
+            direction = "Right";
+            if (position.x + speed >= move.x) {
+                position.x = move.x;
+                Move.poll();
+            } else {
+                position.x += speed;
+            }
+        } else if (position.x > move.x) {
+            direction = "Left";
+            if (position.x - speed <= move.x) {
+                position.x = move.x;
+                Move.poll();
+            } else {
+                position.x -= speed;
+            }
+        } else if (position.y < move.y) {
+            if (position.y + speed >= move.y) {
+                position.y = move.y;
+                Move.poll();
+            } else {
+                position.y += speed;
+            }
+        } else if (position.y > move.y) {
+            if (position.y - speed <= move.y) {
+                position.y = move.y;
+                Move.poll();
+            } else {
+                position.y -= speed;
+            }
+        }
+    }
+
+    @Override
+    public void render(GraphicsContext gc) {
+        int time = (int) (world.time / 1e6);
+        final int circle = 1500 * Sprite.SCALED_SIZE / 96;
+        if (direction.equals("Left")) {
+            img = Sprite.movingSprite(Sprite.doll_left1, Sprite.doll_left2, Sprite.doll_left3, time, circle).getFxImage();
+        } else if (direction.equals("Right")) {
+            img = Sprite.movingSprite(Sprite.doll_right1, Sprite.doll_right2, Sprite.doll_right3, time, circle).getFxImage();
+        }
+        gc.drawImage(img, position.x, position.y);
+    }
+
+    @Override
+    public void destroy() {
+        hp--;
+    }
+}
