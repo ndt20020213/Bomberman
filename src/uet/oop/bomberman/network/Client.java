ad21ad665336ddc1ec4d60a84d1c9c8ff6db4eca
Phan Cong Thanh <phancongthanhvtvpvn@gmail.com>
2021-12-09 17:47:03 +0700

    Update

--- src/uet/oop/bomberman/network/Client.java
+++ src/uet/oop/bomberman/network/Client.java
@@ -166,6 +166,7 @@ public class Client extends Connection {
                         System.exit(0);
                 }
             } catch (Exception e) {
+                break;
             }
         }
     }
